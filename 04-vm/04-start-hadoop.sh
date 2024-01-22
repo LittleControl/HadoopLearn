cf9773f94da91ca5fcd85c02cbab8c55153f6dc7
littlecontrol <i@littlecontrol.me>
2024-01-22 22:58:36 +0800

    feat: start/stop scripts all in one

---
+++ 04-vm/04-start-hadoop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo '======start hdfs ========='
+ssh root@arch0 "/opt/hadoop-3.3.6/sbin/start-dfs.sh"
+echo '======start yarn ========='
+ssh root@arch1 "/opt/hadoop-3.3.6/sbin/start-yarn.sh"
+echo '====start historyserver==='
+ssh root@arch0 "/opt/hadoop-3.3.6/bin/mapred --daemon start historyserver"
---
+++ 04-vm/05-stop-hadoop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo '====stop historyserver==='
+ssh root@arch0 "/opt/hadoop-3.3.6/bin/mapred --daemon stop historyserver"
+echo '======stop yarn ========='
+ssh root@arch1 "/opt/hadoop-3.3.6/sbin/stop-yarn.sh"
+echo '======stop hdfs ========='
+ssh root@arch0 "/opt/hadoop-3.3.6/sbin/stop-dfs.sh"
+
+echo '======stop finished======'
+exit 1
